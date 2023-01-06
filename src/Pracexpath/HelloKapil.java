bf16a336993a62f72b975500f5228d6fe5888a2f
Lenovo <k.talodiya2@gmail.com>
2023-01-06 20:09:35 +0530

    EST Timezone commit code

--- src/Pracexpath/HelloKapil.java
+++ src/Pracexpath/HelloKapil.java
@@ -5,5 +5,9 @@ public class HelloKapil {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World Kapil");
+		System.out.println("Hello World Kapil");
+		System.out.println("Hello World Kapil");
+		System.out.println("Hello World Kapil");
+		System.out.println("Hello World Kapil");
 	}
 }
